c598861ad7477a2f0aaf72f8032e50a6b3633dc5
Nicolas Tito <mrnicolastito@icloud.com>
2022-01-15 16:39:49 +0000

    up

--- imobi/settings.py
+++ imobi/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from django.contrib.messages import constants
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j^d#l#xxv@rd#0%sc^xnups%18+w&-s6u@*)s^+hdp=q(7oi43'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
